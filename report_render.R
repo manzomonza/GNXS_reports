f3465d370945f820094868b1f3e0e20dd374ed4d
manzomonza <massimiliano.manzo@posteo.ch>
2023-01-18 12:17:11 +0100

    Change ComplexHeatmap to pheatmap

--- report_render.R
+++ report_render.R
@@ -6,5 +6,15 @@ option_list = list(
               help="directory containing Genexus sequencing group", metavar="character"))
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser)
+
+
+dir.create(paste0(opt$dir, "/GNXS_QC_report"))
+html_filename = paste0(opt$dir, "/GNXS_QC_report/GNXS_report.html")
+
+# rmarkdown::render('~/USB/USB_Diagnostics/GNXS_reports/report.Rmd',
+#                   params = list(diroi = opt$dir),
+#                   output_file = html_filename)
+
 rmarkdown::render('/home/ionadmin/github_app/GNXS_reports/report.Rmd',
-                  params = list(diroi = opt$dir))
+                  params = list(diroi = opt$dir),
+                  output_file = html_filename)
