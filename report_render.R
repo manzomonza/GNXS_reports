1e217d844d2f4dfa122dae651e4a1bd34570688a
manzomonza <massimiliano.manzo@posteo.ch>
2023-01-18 11:37:14 +0100

    Remove ~xlsx files

---
+++ report_render.R
@@ -0,0 +1,10 @@
+## GNXS report render script
+
+library(optparse)
+option_list = list(
+  make_option(c("-d", "--dir"), type="character", default=NULL,
+              help="directory containing Genexus sequencing group", metavar="character"))
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser)
+rmarkdown::render('/home/ionadmin/github_app/GNXS_reports/report.Rmd',
+                  params = list(diroi = opt$dir))
